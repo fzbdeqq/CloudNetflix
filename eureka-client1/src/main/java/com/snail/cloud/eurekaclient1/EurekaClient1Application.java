44db9ec0e0382320c2771d0285cff8fd7cf9bcf9
fzbdeqq <438267278@qq.com>
2020-10-13 22:19:49 +0800

    hystrix 熔断

--- eureka-client1/src/main/java/com/snail/cloud/eurekaclient1/EurekaClient1Application.java
+++ eureka-client1/src/main/java/com/snail/cloud/eurekaclient1/EurekaClient1Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableTurbine
 public class EurekaClient1Application {
 
     public static void main(String[] args) {
