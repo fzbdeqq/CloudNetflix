903880050f6b1e452f49121b3eee3d97d2b51e39
fzbdeqq <438267278@qq.com>
2020-10-15 23:00:18 +0800

    zuul

--- zuul-demo/src/main/java/com/snail/cloud/zuuldemo/ZuulDemoApplication.java
+++ zuul-demo/src/main/java/com/snail/cloud/zuuldemo/ZuulDemoApplication.java
@@ -12,6 +12,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableHystrix
 public class ZuulDemoApplication {
 //Zuul starter does not include a discovery client
+//if you use @EnableZuulProxy with the Spring Boot Actuator, you enable two additional endpoints:
+// routes、filters
+// http://localhost:8888/actuator/routes/details
     public static void main(String[] args) {
         SpringApplication.run(ZuulDemoApplication.class, args);
     }
